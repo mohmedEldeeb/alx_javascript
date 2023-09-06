386b1844c413979dc4510ab86220c27f09724322
mohmedEldeeb <eldeebmahammed@gmail.com>
2023-09-06 20:38:48 +0300

    fix

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -14,14 +14,13 @@ class Rectangle {
         }
     }
     double(){
-        for (let i = 0; i < this.height * 2; i++) {
-            console.log('X'.repeat(this.width * 2));
-        }
+        this.height = this.height * 2;
+        this.width = this.width * 2;
     }
     repeat(){
-        for (let i = 0; i < this.width; i++) {
-            console.log('X'.repeat(this.height));
-        }
+        r = this.height;
+        this.width = this.height
+        this.height = r
     }
     
 }
