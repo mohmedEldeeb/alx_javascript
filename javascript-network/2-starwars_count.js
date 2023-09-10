90779829c4764941c99e8a7dba61995593a5ac65
mohmedEldeeb <eldeebmahammed@gmail.com>
2023-09-10 20:18:11 +0300

    network

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,3 +1,23 @@
 #!/usr/bin/node
 
 const request = require('request');
+
+
+const url =  process.argv[2];
+
+request(url, function (error, response, body) {
+    if (error) {
+        console.log(error);
+    } else if (response.statusCode === 200) {
+        const films = JSON.parse(body).results;
+        let n = 0;
+        for (const film in films) {
+            if (film.characters.includes('18')) {
+                n++;
+            }
+        }
+        console.log(n);
+    } else {
+        console.log('code: ' + response.statusCode);
+    }
+});
