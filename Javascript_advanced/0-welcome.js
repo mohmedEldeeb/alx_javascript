4b0bc9fac16a5e7c042e784fb399f5e62ee90b02
mohmedEldeeb <eldeebmahammed@gmail.com>
2023-09-15 16:09:01 +0300

    task-7 & 6

--- Javascript_advanced/0-welcome.js
+++ Javascript_advanced/0-welcome.js
@@ -3,7 +3,7 @@
 const welcome = (firstName, lastName) => {
     let fullName = firstName + ' ' + lastName;
     const displayFullName = () => {
-        alert("Welcome, " + fullName);
+        alert("Welcome " + fullName + "!");
     }
     return displayFullName();
 }
